50a9861ed164faf630bc0a470a06ee47fb088c5d
NoahWhiteson <noahwhiteson5@gmail.com>
2026-09-05 11:34:49 -0700

    Initial Commit

---
+++ Safesight/SafesightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafesightApp.swift
+//  Safesight
+//
+//  Created by Noah Whiteson on 2026-09-05.
+//
+
+import SwiftUI
+
+@main
+struct SafesightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
